8edece0be70d09021f6f741512c5fe0651cd7946
jackmacaa <jackmaca@msn.com>
2024-02-10 01:22:36 +1030

    added yoga

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,13 @@
-import { execute, parse } from 'graphql'
-import { schema } from './schema'
- 
-async function main() {
-  const myQuery = parse(/* GraphQL */ `
-    query {
-      hello
-    }
-  `)
- 
-  const result = await execute({
-    schema,
-    document: myQuery
-  })
- 
-  console.log(result)
+import { createServer } from "http";
+import { createYoga } from "graphql-yoga";
+import { schema } from "./schema";
+
+function main() {
+  const yoga = createYoga({ schema });
+  const server = createServer(yoga);
+  server.listen(4000, () => {
+    console.info("Server is running on http://localhost:4000/graphql");
+  });
 }
- 
-main()
+
+main();
